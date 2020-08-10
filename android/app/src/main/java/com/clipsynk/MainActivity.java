53be9b7281d1c1ced42d9708a84c42829db4b998
Rousan Ali <rousanali786@gmail.com>
2020-08-11 01:46:15 +0530

    Update

---
+++ android/app/src/main/java/com/clipsynk/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clipsynk;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ClipSynk";
+  }
+}
