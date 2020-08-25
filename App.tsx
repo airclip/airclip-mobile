f427ea8b080745891712ab393714ef0150a17a5a
Rousan Ali <rousanali786@gmail.com>
2020-08-25 19:55:32 +0530

    Add config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {enableScreens} from 'react-native-screens';
 import {Button, StatusBar, View, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import store from './store';
 
 enableScreens();
@@ -12,28 +14,44 @@ enableScreens();
 const Stack = createStackNavigator();
 
 const HomeScreen = () => {
+  useEffect(() => {
+    Icon.getImageSource('rocket', 20, 'red')
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    console.log(Icon.getFontFamily());
+  }, []);
+
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      <Text>ss</Text>
+      <Text>
+        ss
+        <Icon name="rocket" size={30} color="green" />
+      </Text>
     </View>
   );
 };
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator headerMode="screen">
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{headerStyle: {backgroundColor: 'red'}}}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Provider>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <PaperProvider>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator headerMode="screen">
+              <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{headerStyle: {backgroundColor: 'red'}}}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </PaperProvider>
+    </Provider>
   );
 };
 
